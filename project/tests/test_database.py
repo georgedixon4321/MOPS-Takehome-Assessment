c46f8f7a129592d6f01d505d431400fe51c7a943
George Dixon <george.dixon@dabapps.com>
2022-10-20 06:25:56 +0100

    Began adding database tests

--- project/tests/test_database.py
+++ project/tests/test_database.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+from project.utils.database import VerkadaDB
+
+
+def test_get_verkada_db():
+    dbInstance = VerkadaDB()
+    dbInstance.addTable("TestTable")
+    assert dbInstance.getVerkadaDB() == {"TestTable": {}}
